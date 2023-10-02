9ac36e0ca95bf93ecd7af46ac88c84bca861808b
qvazi <qvazi-s@ya.ru>
2023-10-02 07:42:46 +0000

    Проверка обнуления токена

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { apingwebService } from "../api-sevice/apingweb";
 import { Article } from "../components/Article";
+import { storage } from "../lib/axios";
 
 export const Articles = () => {
   const articlesQuery = useQuery({
@@ -11,6 +12,13 @@ export const Articles = () => {
   return (
     <div>
       <h2>Articles</h2>
+      <button
+        onClick={() => {
+          storage.user = null;
+        }}
+      >
+        Обнулить токен
+      </button>
       <Article id="1" />
       <Article id="2" />
       <Article id="3" />

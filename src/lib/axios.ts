2ba9d8bea8e5f8bd6e0a6606765cedb2de933cea
qvazi <qvazi-s@ya.ru>
2023-10-02 07:39:22 +0000

    Обновление токена при запросе данных

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -11,7 +11,7 @@ service.interceptors.request.use(
   },
   (request) => {
     console.log("service", "intr.req.error");
-    return request;
+    return Promise.reject(request);
   },
 );
 service.interceptors.response.use(
@@ -48,16 +48,16 @@ export const apingwebInstance = axios.create({
 
 apingwebInstance.interceptors.request.use(async (request) => {
   console.log(storage, "storage");
-  // if (storage.user && request.url !== "/login" && request.url !== "/register") {
-  //   request.headers["Authorization"] = "Bearer " + storage.user.token;
-  //   request.headers["Content-Type"] = "application/json";
-  // }
+  if (storage.user && request.url !== "/login" && request.url !== "/register") {
+    request.headers["Authorization"] = "Bearer " + storage.user.token;
+    request.headers["Content-Type"] = "application/json";
+  }
 
   return request;
 });
 
-const awaitingRequests = [];
-let relogin = false;
+let authPromise: Promise<unknown> | undefined;
+let authErrorCount = 0;
 apingwebInstance.interceptors.response.use(
   async (response) => {
     console.log("success intr.res", response.config.url);
@@ -74,15 +74,22 @@ apingwebInstance.interceptors.response.use(
     // console.log(error);
     if (axios.isAxiosError(error)) {
       if (
+        error.config &&
         error.response?.status === 404 &&
         error.config?.url !== "/login" &&
-        error.config?.url !== "/register"
+        error.config?.url !== "/register" &&
+        !error.config._retry
       ) {
-        awaitingRequests.push(error.config);
-        if (!relogin) {
-          try {
-            relogin = true;
-            const response = await apingwebInstance.post(
+        try {
+          error.config._retry = true;
+          if (authErrorCount) {
+            authErrorCount += 1;
+            await authPromise;
+            authErrorCount -= 1;
+            return apingweb(error.config);
+          } else {
+            authErrorCount += 1;
+            authPromise = apingwebInstance.post(
               "/login",
               {
                 email: "qwe@qwe",
@@ -94,17 +101,17 @@ apingwebInstance.interceptors.response.use(
                 },
               },
             );
+            const response = await authPromise;
             storage.user = {
               name: response.data.result.name,
               phone: response.data.result.phone,
               token: response.data.token,
             };
-            relogin = false;
-            awaitingRequests.forEach((config) => apingwebInstance(config));
-            return;
-          } catch (error) {
-            return Promise.reject(error);
+            authErrorCount -= 1;
+            return apingweb(error.config);
           }
+        } catch (error) {
+          return Promise.reject(error);
         }
       }
     }
